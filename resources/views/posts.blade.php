111193f429ca03b0bd19c4b7c1e4b7668c7aeddd
Auguste <adollinger@edenschool.fr>
2025-01-24 10:09:16 +0100

    useless commit

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -10,13 +10,13 @@
     </x-slot>
 
     @foreach($posts as $post)
-    <div class="py-12">
+    <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 flex flex-col gap-5">
                 <h2 class="font-semibold text-xl text-gray-800 leading-tight">{{ $post->title }}</h2>
                 <p>{{ $post->content }}</p>
-                <p class="text-xs italic">Last updated: {{ $post->updated_at->format('M d, Y H:i') }} by {{ $post->user_name }}</p>
-                @if(Auth::user()->admin == true)
+                <p class="text-xs italic">Last updated: {{ $post->updated_at->addHour()->format('M d, Y H:i') }} by {{ $post->user_name }}</p>
+                @if(Auth::user()->admin == true || Auth::user()->id == $post->user_id)
                     <form action="{{ route('posts.destroy', $post->id) }}" method="POST">
                         @csrf
                         @method('DELETE')
